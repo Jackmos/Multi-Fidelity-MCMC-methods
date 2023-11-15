a899cebc94b61ebc22dcd45a56614c91e048542a
mossinel <giacomo.mossinelli@mail.polimi.it>
2023-11-15 01:15:36 +0100

    multiparam(da aggiustare)

--- PACS_project2/Multiparameter/3_params/ann_functions3D.py
+++ PACS_project2/Multiparameter/3_params/ann_functions3D.py
@@ -14,6 +14,9 @@ import tensorflow as tf
 import h5py
 
 
+def custom_activation(x):
+    return x + K.square(K.sin(x))
+
 def import_data(name):
     with h5py.File(name, "r") as file:
         # Ora puoi accedere ai dati all'interno del file
@@ -23,17 +26,14 @@ def import_data(name):
         
         x=file["x"]
         x=x[()]
-        
-        y=file["y"]
-        y=y[()]
-        
+
         U = file["U"]
         U = U[()]
 
         # V=file['V']
         # V=V[()]
 
-    return (R,x,y, U)
+    return (R, U,x)
 def custom_loss(y_pred,y_true):
     goodind = K.not_equal(y_pred,-10)
     #goodind = tf.math.logical_not(tf.math.is_nan(y_pred))
@@ -55,50 +55,49 @@ def getOpti(name,lr):
 
 def getModel(params,name):
     if(name == '2step'):
-        inputs = Input(shape=(4,))
-        hidden1 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2(params['l2weight']),kernel_initializer=params['kernel_init'])(inputs)
+        inputs = Input(shape=(3,))
+        hidden1 = Dense(int(params['nodes']),activation=custom_activation,kernel_regularizer=l2(params['l2weight']),kernel_initializer=params['kernel_init'])(inputs)
         #hidden2 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2(params['l2weight']),kernel_initializer=params['kernel_init'])(hidden1)
-        #hidden2 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2(params['l2weight']),kernel_initializer=params['kernel_init'])(hidden2)
-        #hidden3 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2(params['l2weight']),kernel_initializer=params['kernel_init'])(hidden3)
         output = Dense(1,activation='linear',name='HF')(hidden1)
     elif (name == 'LF'):
-        inputs = Input(shape=(3,))
-        hidden1 = Dense(64,activation='tanh',kernel_initializer=params['kernel_init'],kernel_regularizer=l2(0.001))(inputs)
-        hidden2 = Dense(64,activation='tanh',kernel_initializer=params['kernel_init'],kernel_regularizer=l2(0.001))(hidden1)
-        hidden3 = Dense(64,activation='tanh',kernel_initializer=params['kernel_init'],kernel_regularizer=l2(0.001))(hidden2)
-        hidden4 = Dense(64,activation='tanh',kernel_initializer=params['kernel_init'],kernel_regularizer=l2(0.001))(hidden3)
+        inputs = Input(shape=(2,))
+        hidden1 = Dense(64,activation=custom_activation,kernel_initializer=params['kernel_init'],kernel_regularizer=l2(0.001))(inputs)
+        hidden2 = Dense(64,activation=custom_activation,kernel_initializer=params['kernel_init'],kernel_regularizer=l2(0.001))(hidden1)
+        hidden3 = Dense(64,activation=custom_activation,kernel_initializer=params['kernel_init'],kernel_regularizer=l2(0.001))(hidden2)
+        hidden4 = Dense(64,activation='sigmoid',kernel_initializer=params['kernel_init'],kernel_regularizer=l2(0.001))(hidden3)
         output = Dense(1,activation='linear',name='LF')(hidden4)
     elif (name == 'HF'):
-        inputs = Input(shape=(3,))
-        hidden1 = Dense(64,activation='tanh',kernel_initializer=params['kernel_init'],kernel_regularizer=l2(0.001))(inputs)
-        output = Dense(1,activation='linear',name='LF')(hidden1)        
+        inputs = Input(shape=(2,))
+        hidden1 = Dense(64,activation=custom_activation,kernel_initializer=params['kernel_init'],kernel_regularizer=l2(0.001))(inputs)
+        hidden1=Dropout(0.5)(hidden1)
+        output = Dense(1,activation='linear',name='LF')(hidden1) 
     elif (name == 'Single'):
-        inputs = Input(shape=(3,))
-        hidden1 = Dense(int(params['nodes']),activation='tanh',kernel_initializer=params['kernel_init'],kernel_regularizer=l2(params['l2weight']))(inputs)
-        hidden2 = Dense(int(params['nodes']),activation='tanh',kernel_initializer=params['kernel_init'],kernel_regularizer=l2(params['l2weight']))(hidden1)
+        inputs = Input(shape=(2,))
+        hidden1 = Dense(int(params['nodes']),activation=custom_activation,kernel_initializer=params['kernel_init'],kernel_regularizer=l2(params['l2weight']))(inputs)
+        hidden2 = Dense(int(params['nodes']),activation=custom_activation,kernel_initializer=params['kernel_init'],kernel_regularizer=l2(params['l2weight']))(hidden1)
         #hidden3 = Dense(int(params['nodes']),activation='tanh',kernel_initializer=params['kernel_init'],kernel_regularizer=l2(params['l2weight']))(hidden2)
         #hidden4 = Dense(int(params['nodes']),activation='tanh',kernel_initializer=params['kernel_init'],kernel_regularizer=l2(params['l2weight']))(hidden3)
         output = Dense(1,activation='linear',name='Single')(hidden2)
 
     elif (name == 'Hflin'):
-        inputs = Input(shape=(4,))
+        inputs = Input(shape=(3,))
         hiddenlin = Dense(64,activation='linear',kernel_regularizer=l2(params['l2weight']),kernel_initializer=params['kernel_init'])(inputs)
         output = Dense(1,activation='linear',name='HFlin')(hiddenlin)
 
     elif(name == '3step'):
-        inputs = Input(shape=(5,))
-        hidden1 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2(params['l2weight']),kernel_initializer=params['kernel_init'])(inputs)
+        inputs = Input(shape=(4,))
+        hidden1 = Dense(int(params['nodes']),activation=custom_activation,kernel_regularizer=l2(params['l2weight']),kernel_initializer=params['kernel_init'])(inputs)
         #hidden2 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2(params['l2weight']),kernel_initializer=params['kernel_init'])(hidden1)
         output = Dense(1,activation='linear',name='HF')(hidden1)
 
     elif (name == 'GP'):
-        inputs = Input(shape=(3,))
-        hidden1 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(inputs)
-        hidden2 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden1)
-        hidden3 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden2)
-        hidden4 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden3)
-        GPlayer = Dense(2,activation='linear',kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden4)
-        outputLF = Dense(1,activation='linear',name='LF')(GPlayer)
+        inputs = Input(shape=(2,))
+        hidden1 = Dense(int(params['nodes']),activation=custom_activation,kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(inputs)
+        hidden2 = Dense(int(params['nodes']),activation=custom_activation,kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden1)
+        hidden3 = Dense(int(params['nodes']),activation=custom_activation,kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden2)
+        hidden4 = Dense(int(params['nodes']),activation=custom_activation,kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden3)
+        GPlayer = Dense(2,activation=custom_activation,kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden4)
+        outputLF = Dense(1,activation=custom_activation,name='LF')(GPlayer)
         outputHF = Dense(1,activation='linear',name='HF')(GPlayer)
         output = [outputHF,outputLF]
         model = Model(inputs=inputs, outputs=output)
@@ -107,14 +106,14 @@ def getModel(params,name):
         return model
 
     elif (name == 'Inter'):
-        inputs = Input(shape=(3,))
-        hidden1 = Dense(64,activation='tanh',kernel_initializer=params['kernel_init'],kernel_regularizer=l2((1-params['alpha'])*params['l2weight']))(inputs)
-        hidden2 = Dense(64,activation='tanh',kernel_initializer=params['kernel_init'],kernel_regularizer=l2((1-params['alpha'])*params['l2weight']))(hidden1)
-        outputLF = Dense(1,activation='linear',name='LF')(hidden2)
-        outputadd = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden2)
+        inputs = Input(shape=(2,))
+        hidden1 = Dense(64,activation=custom_activation,kernel_initializer=params['kernel_init'],kernel_regularizer=l2((1-params['alpha'])*params['l2weight']))(inputs)
+        hidden2 = Dense(64,activation=custom_activation,kernel_initializer=params['kernel_init'],kernel_regularizer=l2((1-params['alpha'])*params['l2weight']))(hidden1)
+        outputLF = Dense(1,activation=custom_activation,name='LF')(hidden2)
+        outputadd = Dense(int(params['nodes']),activation=custom_activation,kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden2)
         merge = concatenate([outputLF,outputadd])
-        hidden3 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(merge)
-        hidden4 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden3)
+        hidden3 = Dense(int(params['nodes']),activation=custom_activation,kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(merge)
+        hidden4 = Dense(int(params['nodes']),activation=custom_activation,kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden3)
         #hidden5 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden4)
         #hidden6 = Dense(int(params['nodes']),activation='tanh',kernel_regularizer=l2((1-params['alpha'])*params['l2weight']),kernel_initializer=params['kernel_init'])(hidden5)
 
